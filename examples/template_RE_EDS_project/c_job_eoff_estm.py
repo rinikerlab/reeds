664f768cff761f3574fc3af75eb647afcd92774f
Candide Champion <candide.champion@phys.chem.ethz.ch>
2021-04-07 15:06:59 +0200

    Updated the default submit masterscript

--- examples/template_RE_EDS_project/c_job_eoff_estm.py
+++ examples/template_RE_EDS_project/c_job_eoff_estm.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import os, sys, glob
-from reeds.modules import do_RE_EDS_eoffEstimation as eoffEstm
+import reeds
+import pygromos
 
+from reeds.modules import do_RE_EDS_eoffEstimation as eoffEstm
 
 sys.path.append(os.getcwd())
 
@@ -10,23 +12,29 @@ from global_definitions import name, root_dir
 from global_definitions import gromosXX_bin, gromosPP_bin, ene_ana_lib
 from global_definitions import in_top_file, in_pert_file, in_disres_file, in_template_reeds_imd
 
-
-
 #STEP specifics
-out_eoff_dir = root_dir+"/c_eoff"
-next_lowerBound_dir = root_dir+"/b_lowerBound/analysis/next"
-in_name = name+"_energy_offsets"
-
+out_eoff_dir = root_dir + "/c_energy_offsets"
+sval_file = "/path/to/analysis/next/s_vals.csv"
+in_name = name + "_energy_offsets"
 
 ##make folder
 out_eoff_dir = bash.make_folder(out_eoff_dir)
 
+opt_states = "/path/to/a_optimizedState/analysis/data"
+template_imd = "/home/cchampion/tmp/test_area/CHK1_ring/input/template_reeds.imd"
+
 #In- Files
 topology = fM.Topology(top_path=in_top_file, disres_path=in_disres_file, pertubation_path=in_pert_file)
-coords  =glob.glob(next_lowerBound_dir+"/*.cnf")
+coords = glob.glob(opt_states + "/*.cnf")
 system = fM.System(coordinates=coords, name=in_name, top=topology)
 print(system)
 
 last_jobID = eoffEstm.do(out_root_dir=out_eoff_dir, in_simSystem=system,
                          gromosXX_bin_dir = gromosXX_bin, gromosPP_bin_dir = gromosPP_bin,
-                         in_template_imd_path=in_template_reeds_imd, in_ene_ana_lib=ene_ana_lib)
+                         sval_file = sval_file,
+                         in_template_imd_path = template_imd,
+                         in_ene_ana_lib=ene_ana_lib, 
+                         submit=True, 
+                         optimized_states = opt_states,                                     
+                        )
+
