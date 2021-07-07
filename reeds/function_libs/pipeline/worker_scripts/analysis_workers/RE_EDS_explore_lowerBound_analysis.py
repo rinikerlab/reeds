8092d2e3c2db85232e5f821bdde68e70ebf86163
schroederB <benjamin-ries@outlook.com>
2021-07-07 14:52:25 +0200

    fixing indexing, such the u_idx-1 s-value does not appear twice

--- reeds/function_libs/pipeline/worker_scripts/analysis_workers/RE_EDS_explore_lowerBound_analysis.py
+++ reeds/function_libs/pipeline/worker_scripts/analysis_workers/RE_EDS_explore_lowerBound_analysis.py
@@ -176,11 +176,11 @@ def do(out_analysis_dir: str, system_name: str,
     print('New s distribution will place ' + str(num_states) + ' replicas between  s = ' + str(s_values[u_idx]) + ' and s = ' +str(s_values[u_idx+3]))
 
     if(not final_number_of_replicas is None):
-        new_sdist = list(s_log_dist.get_log_s_distribution_between(s_values[0],s_values[u_idx-1], final_number_of_replicas-num_states))
+        new_sdist = list(s_log_dist.get_log_s_distribution_between(s_values[0],s_values[u_idx-2], final_number_of_replicas-num_states))
         lower_sdist = list(s_log_dist.get_log_s_distribution_between(s_values[u_idx-1], s_values[u_idx], num_states))
         new_sdist.extend(lower_sdist)
     else:
-        new_sdist = s_values[:u_idx-1]
+        new_sdist = s_values[:u_idx-2]
         lower_sdist = s_log_dist.get_log_s_distribution_between(s_values[u_idx-1], s_values[u_idx], num_states)
         new_sdist.extend(lower_sdist)
 
