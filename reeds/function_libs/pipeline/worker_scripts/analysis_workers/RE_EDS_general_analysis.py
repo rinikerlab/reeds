9e0f9c2008040944b78e60f095c9b105d7389b59
Candide Champion <cchampion@phys.chem.ethz.ch>
2022-05-06 16:25:58 +0200

    fix formatting + reduce output cluter

--- reeds/function_libs/pipeline/worker_scripts/analysis_workers/RE_EDS_general_analysis.py
+++ reeds/function_libs/pipeline/worker_scripts/analysis_workers/RE_EDS_general_analysis.py
@@ -447,10 +447,10 @@ def do_Reeds_analysis(in_folder: str, out_folder: str, gromos_path: str,
 
             # Decrement the value of undersampling_idx by 1. As indexing followed a different convention. 
             new_eoffs_estm, all_eoffs = eds_energy_offsets.estimate_energy_offsets(ene_trajs = energy_trajectories, initial_offsets = Eoff[0], sampling_stat=sampling_results, s_values = s_values,
-                                                                              out_path = out_dir, temp = temp, trim_beg = 0., undersampling_idx = sampling_results['undersamplingThreshold']-1,
-
-                                                        plot_results = True, calc_clara = False)
-            print("ENERGY OFF: ", new_eoffs_estm, all_eoffs)
+                                                                                   out_path = out_dir, temp = temp, trim_beg = 0., undersampling_idx = sampling_results['undersamplingThreshold']-1,
+                                                                                   plot_results = True, calc_clara = False)
+            print("ENERGY OFFSETS ESTIMATION:\n") 
+            print("new_eoffs_estm: " + str(np.round(new_eoffs_estm, 2)))
         elif(sub_control["eoffset_rebalancing"]):
             new_eoffs_rb = rebalance_eoffs_directCounting(sampling_stat=sampling_results['samplingDistributions'], old_eoffs=Eoff,
                                                        learningFactor=eoffRebalancing_learningFactor, pseudo_count=eoffRebalancing_pseudocount,
