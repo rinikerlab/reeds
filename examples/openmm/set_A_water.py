42eadaa003570db6e35063dc2e73d3081c411692
Salomé Rieder <salome.rieder@solnet.ch>
2022-05-17 14:56:24 +0200

    add examples for openmm

--- examples/openmm/set_A_water.py
+++ examples/openmm/set_A_water.py
@@ -1,3 +1,4 @@
+import os
 from reeds.openmm.reeds_openmm import *
 
 os.makedirs("water", exist_ok = True)
@@ -9,10 +10,10 @@ energy_offsets = np.array([0.0, -25.13, -94.15, 105.73, 122.62, 139.76])
 # Careful: taken from Restraintmaker, so indices start at one, and will be decreased by one during the assignment of the distance restraints!!
 restraint_pairs = [[31,78], [34,75], [35,80], [32,77], [16,52], [19,49], [18,54], [15,51], [49,78], [52,75], [53,80], [50,77], [1,32], [4,35], [5,34], [2,31], [16,64], [19,67], [18,66], [15,63], [2,67], [5,64], [4,65], [1,68]]
 
-reeds_simulation_variables = ReedsSimulationVariables(s_values, energy_offsets, restraint_pairs, total_steps = 20, distance_restraints_start_at_1 = True)
+reeds_simulation_variables = ReedsSimulationVariables(s_values, energy_offsets, restraint_pairs, total_steps = 250000, distance_restraints_start_at_1 = True)
 
-param_file = "input/all_ligands_solv.leap.prm"
-crd_files = "input/all_ligands_solv.leap.crd"
+param_file = "../input/all_ligands_solv.leap.prm"
+crd_files = "../input/all_ligands_solv.leap.crd"
 #state_files = ["0/hyd_set_water_state_replica_" + str(i) for i in range(len(s_values))]
 reeds_input_files = ReedsInputFiles(param_file, crd_files)
 
