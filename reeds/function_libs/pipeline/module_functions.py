257c5054907c2cb2817ae1cd82d60eb6b89115af
eruijsena <105713339+eruijsena@users.noreply.github.com>
2022-06-03 16:28:25 +0200

    Generate sopt plots for all optimizations, even when not doing sopt

--- reeds/function_libs/pipeline/module_functions.py
+++ reeds/function_libs/pipeline/module_functions.py
@@ -345,9 +345,9 @@ def build_optimization_step_dir(iteration: int,  iteration_folder_prefix: str, p
 
     ##which analysis functions to execute
     control_dict = {  # this dictionary is controlling the post  Simulation analysis procedure!
-        "sopt": {"do": run_NLRTO or run_NGRTO,
+        "sopt": {"do": True,
                  "sub": {
-                     "run_RTO": True,
+                     "run_RTO": run_NGRTO or run_NLRTO,
                      "run_NLRTO": run_NLRTO,
                      "run_NGRTO": run_NGRTO,
                      "visualize_transitions": True,
