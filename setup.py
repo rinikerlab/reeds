44f73c666b33021d4073ebe6f96d693e91f5913c
eruijsena <enricoruijsenaars@gmail.com>
2024-02-18 11:11:10 +0100

    remove versioneer

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ The aim of the module is to make the RE-EDS pipeline accesible to everyone! :)
 """
 import sys
 from setuptools import setup, find_packages
-import versioneer
 
 short_description = __doc__.split("\n")
 
@@ -27,8 +26,6 @@ setup(
     description=short_description[0],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
     license='MIT',
 
     # Which Python importable modules should be included when your package is installed
